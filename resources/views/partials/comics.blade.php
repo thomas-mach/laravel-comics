75f3642afeeaffaad7cb94e4926dc199d27b5d82
Tomasz Mach <m4chtomasz@gmail.com>
2024-05-21 16:37:55 +0200

    Add: yeld to title sites

--- resources/views/partials/comics.blade.php
+++ resources/views/partials/comics.blade.php
@@ -1,5 +1,7 @@
 @extends('layouts.app')
 
+@section('title', 'Comics')
+
 @section('content')
 <h1>THIS IS COMICS</h1>
 @endsection
--- resources/views/partials/contacts.blade.php
+++ resources/views/partials/contacts.blade.php
@@ -1,5 +1,7 @@
 @extends('layouts.app')
 
+@section('title','Contacts')
+
 @section('content')
 <h1>THIS IS CONTACTS</h1>
 @endsection
